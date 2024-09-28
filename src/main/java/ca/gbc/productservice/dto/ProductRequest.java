2d4b4537fcba74920dd1c4df056d049e1ed157fa
karanparmar03 <101427974georgebrown.ca>
2024-09-28 13:58:21 -0400

    Resolved merge conflicts

---
+++ src/main/java/ca/gbc/productservice/dto/ProductRequest.java
@@ -0,0 +1,4 @@
+package ca.gbc.productservice.dto;
+
+public record ProductRequest() {
+}
--- src/test/java/ca/gbc/productservice/MicroservicesParentApplicationTests.java
+++ src/test/java/ca/gbc/productservice/MicroservicesParentApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.microservicesparent;
+package ca;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
